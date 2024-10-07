a102e18ece430c4a3b4af30c023d7f2ef1c0e49b
rrumana <ryan.rumana@gmail.com>
2024-10-07 12:36:30 -0700

    Fist significant update in a while

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,128 @@
 use std::collections::HashMap;
+use clap::Parser;
+use anyhow::{anyhow, Result};
+use std::fmt;
+use std::fmt::Display;
+
+/// Simple CLI to-do list
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The action to perform; add, complete, remove, or print
+    #[arg(short, long)]
+    action: String,
+
+    /// The description of the item
+    #[arg(
+        short, 
+        long, 
+        required_if_eq_any([
+            ("action","add"),
+            ("action","complete"),
+            ("action","remove")
+        ]))]
+    description: Option<String>,
+}
 
 struct Todo {
-    // use rust built in HashMap to store key - val pairs
     map: HashMap<String, String>,
 }
 
+impl Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (key, value) in &self.map {
+            write!(f, "{}: {}\n", key, value)?;
+        }
+        Ok(())
+    }
+}
+
 impl Todo {
-    fn new() -> Result<Todo, std::io::Error> {
-        // open db.json
-        let f = std::fs::OpenOptions::new()
+    fn new() -> Result<Todo> {
+        let file = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
             .open("db.json")?;
-        // serialize json as HashMap
-        match serde_json::from_reader(f) {
+
+        match serde_json::from_reader(file) {
             Ok(map) => Ok(Todo { map }),
             Err(e) if e.is_eof() => Ok(Todo {
                 map: HashMap::new(),
             }),
-            Err(e) => panic!("An error occurred: {}", e),
+            Err(e) => Err(anyhow!("Error reading the database from the file: {}", e)),
         }
     }
 
-    fn insert(&mut self, key: String) {
-        // insert a new item into our map.
-        // we pass true as value
+    fn insert(&mut self, key: String) -> Result<()> {
         self.map.insert(key, String::from("To Do"));
+        self.save()?;
+        Ok(())
     }
 
-    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
-        // open db.json
+    fn save(&self) -> Result<()> {
         let f = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .truncate(true)
             .open("db.json")?;
-        // write to file with serde
+        
         serde_json::to_writer_pretty(f, &self.map)?;
         Ok(())
     }
 
-    fn complete(&mut self, key: &String) -> Option<()> {
-        // search the hashmap for the key
-        match self.map.get_mut(key) {
-            Some(v) => {
-                *v = String::from("Done");
-                Some(())
-                },
-            None => None,
+    fn complete(&mut self, key: String) -> Result<()> {
+        match self.map.get_mut(&key) {
+            None => return Err(anyhow!("Could not find key: {}", key)),
+            Some(_) => self.save()?,
         }
+        Ok(())
     }
 
-    fn contains(&mut self, key: &String) -> bool {
-        // Return true if the key exists, false otherwise
-        self.map.contains_key(key)
-    }
-
-    fn print(&mut self, key: &String) {
-        // printing the hashmap based on input string
-        println!("Printing {} items", key);
-        if key == "To Do" {
-            for (key, value) in &self.map {
-                if value == "To Do" {
-                    println!("{}: {}", key, value);
-                }
-            }
-        } else if key == "Done" {
-            for (key, value) in &self.map {
-                if value == "Done" {
-                    println!("{}: {}", key, value);
-                }
-            }
-        } else if key == "List" {
-            for (key, value) in &self.map {
-                println!("{}: {}", key, value);
-            }
-        } else {
-            println!("{} is not a valid print keyord", key);
+    fn remove(&mut self, key: String) -> Result<()> {
+        match self.map.remove(&key) {
+            None => return Err(anyhow!("Could not find key: {}", key)),
+            Some(_) => self.save()?,
         }
+        Ok(())
     }
 }
 
+fn handle_commands(
+    action: String, 
+    description: String, 
+    mut list: Todo,
+) -> Result<()> {
+    match action.as_str() {
+        "add" => list.insert(description)?,
+        "complete" => list.complete(description)?,
+        "remove" => list.remove(description)?,
+        "print" => { 
+            println!("{}", list); 
+            return Ok(()) 
+        },
+        _ => panic!("Invalid command"),
+    }
+
+    Ok(())
+}
+
 fn main() {
-    let action = std::env::args().nth(1).expect("Please specify an action");
-    let item = std::env::args().nth(2).expect("Please specify an item");
+    let args = Args::parse();
 
-    let mut todo = Todo::new().expect("Initialisation of db failed");
+    let action = args.action;
+    let description = match args.description {
+        Some(desc) => desc,
+        None => String::new(),
+    };
 
-    if action == "add" {
-        todo.insert(item);
-        match todo.save() {
-            Ok(_) => println!("todo saved! Item added."),
-            Err(why) => println!("An error occurred: {}", why),
-        }
-    } else if action == "complete" {
-        match todo.complete(&item) {
-            None => println!("'{}' is not present in the list", item),
-            Some(_) => match todo.save() {
-                Ok(_) => println!("todo saved! Item removed."),
-                Err(why) => println!("An error occurred: {}", why),
-            },
-        }
-    } else if action == "show" {
-        match todo.contains(&item) {
-            false => println!("'{}' is not present in the list", item),
-            true => println!("'{}' is present in the list", item)
-        }
-    } else if action == "print" {
-        todo.print(&item)
+    let list = match Todo::new() {
+        Ok(list) => list,
+        Err(e) => panic!("Database initialization failed: {}", e),
+    };
+
+    match handle_commands(action, description, list) {
+        Err(err) => println!("An error occurred executing command: {}", err),
+        _ => (),
     }
 }
